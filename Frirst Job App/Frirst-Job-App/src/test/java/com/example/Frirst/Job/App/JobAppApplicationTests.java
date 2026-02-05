d5c0ac4109d198cbf1f4f5ba82a23cd2943f77ad
kemji002 <kenji.main.mail.002@gmail.com>
2026-02-06 01:56:16 +0900

    feat: Spring Bootアプリケーションの初期設定と基本構造を追加

---
+++ Frirst Job App/Frirst-Job-App/src/test/java/com/example/Frirst/Job/App/JobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Frirst.Job.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

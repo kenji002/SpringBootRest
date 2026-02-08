1a78bae81e6d04f32daba277c05c1879102afba2
kemji002 <kenji.main.mail.002@gmail.com>
2026-02-08 17:11:07 +0900

    feat: レビュー管理のためのReviewServiceインターフェースとCRUD操作を追加しました。

--- job-app/src/main/java/com/example/job_app/reviews/ReviewService.java
+++ job-app/src/main/java/com/example/job_app/reviews/ReviewService.java
@@ -1,5 +1,15 @@
 package com.example.job_app.reviews;
 
-public interface ReviewService {
+import java.util.List;
 
+public interface ReviewService {
+    List<Review> getAllReviews(Long companyId);
+
+    Review addReview(Long companyId, Review review);
+
+    Review getReview(Long companyId, Long reviewId);
+
+    Review deleteReview(Long companyId, Long reviewId);
+
+    Review updateReview(Long companyId, Long reviewId, Review review);
 }
